6e108e0e3dbb9ffae84126849536f752dea4054d
Sean Lee <weilonge@gmail.com>
2015-08-04 09:11:50 +0200

    Fix the issue of incorrect RESTful API behavior.

--- serviceworkers/serviceworkers_v1/worker.js
+++ serviceworkers/serviceworkers_v1/worker.js
@@ -1,5 +1,6 @@
 // The SW will be shutdown when not in use to save memory,
 // be aware that any global state is likely to disappear
+importScripts('lib/URI.js');
 console.log('SW startup');
 
 self.addEventListener('install', function(event) {
@@ -11,10 +12,9 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('Caught a fetch!');
-  if (event.request.url.indexOf('index.html') != -1) {
-    event.respondWith(fetch(event.request.url));
-  } else {
+  var requestUrl = new URI(event.request.url);
+  console.log('Caught a fetch! ' + requestUrl);
+  if (requestUrl.path.indexOf('/API-v1/') === 0) {
     event.respondWith(
       new Response('{"message":"Hello World!' + event.request.url + '"}'),
       {
